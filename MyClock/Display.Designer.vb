b9f787c1c3605fba6170131b2f1c3b6288f3e09b
gitSshiori <sttr01230@gmail.com>
2018-06-13 10:27:46 +0900

    UI fix

---
+++ MyClock/Display.Designer.vb
@@ -0,0 +1,292 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Display
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Display))
+        Dim ListViewItem5 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem("")
+        Dim ListViewItem6 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem("")
+        Dim ListViewItem7 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem("")
+        Dim ListViewItem8 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem("")
+        Me.timeNow = New System.Windows.Forms.Label()
+        Me.secTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ListView2 = New System.Windows.Forms.ListView()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.DirectorySearcher1 = New System.DirectoryServices.DirectorySearcher()
+        Me.ServiceController1 = New System.ServiceProcess.ServiceController()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.PerformanceCounter1 = New System.Diagnostics.PerformanceCounter()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PerformanceCounter1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'timeNow
+        '
+        Me.timeNow.BackColor = System.Drawing.Color.White
+        Me.timeNow.Cursor = System.Windows.Forms.Cursors.Help
+        Me.timeNow.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.timeNow.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.timeNow.Font = New System.Drawing.Font("MV Boli", 36.0!, CType((((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
+                Or System.Drawing.FontStyle.Underline) _
+                Or System.Drawing.FontStyle.Strikeout), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Document, CType(1, Byte), True)
+        Me.timeNow.ForeColor = System.Drawing.Color.DodgerBlue
+        Me.timeNow.Image = CType(resources.GetObject("timeNow.Image"), System.Drawing.Image)
+        Me.timeNow.ImageAlign = System.Drawing.ContentAlignment.TopRight
+        Me.timeNow.Location = New System.Drawing.Point(0, 519)
+        Me.timeNow.Margin = New System.Windows.Forms.Padding(13, 0, 13, 0)
+        Me.timeNow.Name = "timeNow"
+        Me.timeNow.Size = New System.Drawing.Size(977, 88)
+        Me.timeNow.TabIndex = 0
+        Me.timeNow.Text = "12時34分 56秒"
+        Me.timeNow.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        '
+        'secTimer
+        '
+        Me.secTimer.Enabled = True
+        Me.secTimer.Interval = 1000
+        '
+        'Button1
+        '
+        Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button1.Font = New System.Drawing.Font("Monotype Corsiva", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button1.Location = New System.Drawing.Point(858, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(119, 519)
+        Me.Button1.TabIndex = 1
+        Me.Button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.UseWaitCursor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 72
+        Me.ListBox1.Location = New System.Drawing.Point(235, 3)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 76)
+        Me.ListBox1.TabIndex = 2
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(3, 106)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(282, 180)
+        Me.ListView1.TabIndex = 3
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(3, 3)
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(100, 79)
+        Me.MaskedTextBox1.TabIndex = 4
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ListView2
+        '
+        Me.ListView2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListView2.Items.AddRange(New System.Windows.Forms.ListViewItem() {ListViewItem5, ListViewItem6, ListViewItem7, ListViewItem8})
+        Me.ListView2.Location = New System.Drawing.Point(3, 75)
+        Me.ListView2.Name = "ListView2"
+        Me.ListView2.Size = New System.Drawing.Size(532, 196)
+        Me.ListView2.TabIndex = 5
+        Me.ListView2.UseCompatibleStateImageBehavior = False
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(109, 3)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 79)
+        Me.NumericUpDown1.TabIndex = 6
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Maroon
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(977, 607)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(361, 3)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(100, 23)
+        Me.ProgressBar1.TabIndex = 8
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(3, 386)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(463, 76)
+        Me.RadioButton1.TabIndex = 9
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "RadioButton1"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(9, 144)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(144, 66)
+        Me.RichTextBox1.TabIndex = 11
+        Me.RichTextBox1.Text = ""
+        '
+        'ToolTip1
+        '
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Location = New System.Drawing.Point(467, 3)
+        Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.Size = New System.Drawing.Size(121, 97)
+        Me.TreeView1.TabIndex = 12
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(241, 97)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(144, 113)
+        Me.WebBrowser1.TabIndex = 13
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.BackColor = System.Drawing.Color.Snow
+        Me.FlowLayoutPanel1.Controls.Add(Me.MaskedTextBox1)
+        Me.FlowLayoutPanel1.Controls.Add(Me.NumericUpDown1)
+        Me.FlowLayoutPanel1.Controls.Add(Me.ListBox1)
+        Me.FlowLayoutPanel1.Controls.Add(Me.ProgressBar1)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TreeView1)
+        Me.FlowLayoutPanel1.Controls.Add(Me.ListView1)
+        Me.FlowLayoutPanel1.Controls.Add(Me.GroupBox1)
+        Me.FlowLayoutPanel1.Controls.Add(Me.RadioButton1)
+        Me.FlowLayoutPanel1.ImeMode = System.Windows.Forms.ImeMode.AlphaFull
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(12, 12)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(840, 559)
+        Me.FlowLayoutPanel1.TabIndex = 14
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ListView2)
+        Me.GroupBox1.Controls.Add(Me.RichTextBox1)
+        Me.GroupBox1.Controls.Add(Me.WebBrowser1)
+        Me.GroupBox1.Location = New System.Drawing.Point(291, 106)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(538, 274)
+        Me.GroupBox1.TabIndex = 14
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'DirectorySearcher1
+        '
+        Me.DirectorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01")
+        Me.DirectorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01")
+        Me.DirectorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01")
+        '
+        'Display
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(38.0!, 72.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SlateGray
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(977, 607)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.timeNow)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Font = New System.Drawing.Font("MS UI Gothic", 36.0!)
+        Me.ForeColor = System.Drawing.Color.Blue
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.HelpButton = True
+        Me.KeyPreview = True
+        Me.Margin = New System.Windows.Forms.Padding(13)
+        Me.MaximizeBox = False
+        Me.Name = "Display"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show
+        Me.Text = "MyClock"
+        Me.TopMost = True
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.FlowLayoutPanel1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.PerformanceCounter1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents timeNow As Label
+    Friend WithEvents secTimer As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents ListView2 As ListView
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents TreeView1 As TreeView
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents DirectorySearcher1 As DirectoryServices.DirectorySearcher
+    Friend WithEvents ServiceController1 As ServiceProcess.ServiceController
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents PerformanceCounter1 As PerformanceCounter
+End Class
---
+++ MyClock/Display.vb
@@ -0,0 +1,27 @@
+﻿Public Class Display
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles timeNow.Click
+
+    End Sub
+
+    Private Sub Display_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Gray
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.BackColor = Color.Crimson
+
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+
+    End Sub
+
+    Private Sub ToolTip1_Popup(sender As Object, e As PopupEventArgs) Handles ToolTip1.Popup
+
+    End Sub
+
+    Private Sub FlowLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles FlowLayoutPanel1.Paint
+
+    End Sub
+End Class
